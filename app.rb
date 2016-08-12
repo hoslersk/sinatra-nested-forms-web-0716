861685c533ce0855c64f1e76fcb48b44831525a5
Schyler Hosler <schylerhosler@gmail.com>
2016-08-12 17:54:31 -0400

    Initial commit:

--- app.rb
+++ app.rb
@@ -4,6 +4,29 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    # post '/new' do
+    #   erb :show
+    # end
+
+    post '/pirates' do
+      #binding.pry
+      @pirate = Pirate.new(params[:pirate])
+        params[:pirate][:ships].each do |details|
+          Ship.new(details)
+        end
+        @ships = Ship.all
+      erb :'pirates/show'
+    end
+
+    # {"pirate"=>{"name"=>"a", "weight"=>"b", "height"=>"c", "ships"=>[{"name"=>"d", "type"=>"e", "booty"=>"f"}, {"name"=>"g", "type"=>"h", "booty"=>"i"}]}}
 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,24 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
+  def self.clear
+    PIRATES.clear
+  end
+
 end
+
+# Create two classes, a Pirate class and a Ship class. Pirates should have a name, weight, and height. You will also need a class method that returns all the pirates. The ship class should have name, type, and booty attributes, as well as a class method .all that returns all the ships and a class method .clear that deletes all ships.
